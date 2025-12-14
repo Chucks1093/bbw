cc75bceec6a1c6253e80c03c055b21ff7183515d
Anioke Sebastian <aniokechukwudi7@gmail.com>
2025-12-14 23:32:52 +0100

    add Home component and update router to use it

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Home from './pages/Home';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <div>My App</div>,
+		element: <Home />,
 	},
 ]);
 
